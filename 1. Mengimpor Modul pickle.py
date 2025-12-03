64c70c09912520a8ed1ede375669dff07bab4747
rendhive <rendhivecode@gmail.com>
2025-12-03 12:58:59 +0700

    [DOCT] Sample Library Python: 1. Mengimpor Modul pickle.py

---
+++ 1. Mengimpor Modul pickle.py
@@ -0,0 +1,5 @@
+import pickle
+
+print("Modul pickle berhasil diimpor.")
+# Fungsi: Mengimpor modul untuk serialisasi dan deserialisasi objek Python.
+# Kondisi: Digunakan sebelum memanfaatkan fungsionalitas dari pickle.
